28dadf96159208f81e0d4e658808905c46ff7caf
muhammadaminbek2005 <87560397+muhammadaminbek2005@users.noreply.github.com>
2021-11-17 21:41:22 +0000

    tz-9

--- js/script.js
+++ js/script.js
@@ -10,11 +10,16 @@ $(document).ready(function () {
 
 const navbor = () => {
   const burger = document.querySelector('.header__burger');
+  const Close = document.querySelector('.header__close');
   const nav = document.querySelector('.header__menu');
   const navLinks = document.querySelectorAll(".header__menu div")
   // togle
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active")
   })
+  Close.addEventListener("click", () => {
+    nav.classList.toggle("nav-active")
+  })
 }
 navbor ();
+
