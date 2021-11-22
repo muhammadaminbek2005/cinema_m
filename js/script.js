00b220fc0c6b220a7a19a0403f2990a479887ea0
muhammadaminbek2005 <87560397+muhammadaminbek2005@users.noreply.github.com>
2021-11-22 16:19:26 +0000

    tz-16

--- js/script.js
+++ js/script.js
@@ -13,12 +13,12 @@ $(document).ready(function () {
       360: {
         items: 2,
         nav: false,
-        margin:100,
+        margin: 100,
       },
       768: {
         items: 2,
         nav: true,
-        margin:10,
+        margin: 10,
       },
       1024: {
         items: 3,
@@ -42,16 +42,28 @@ const navbor = () => {
   });
 };
 navbor();
-// for table
-let films = [
+//! for table
+filmsDescription = [
   {
     time: "10:00",
     name: "Человек-паук",
     genre: "Фантастика, боевик, приключения",
   },
   {
+    time: "18:00",
+    name: "Человек-паук 2",
+    genre: "Фантастика, боевик, приключения",
+  },
+  {
+    adult: true,
     time: "12:00",
     name: "Собачья жизнь 2",
+    genre: "Фэнтэзи, драма, комедия",
+  },
+  {
+    adult: true,
+    time: "20:00",
+    name: "Собачья жизнь",
     genre: "Фэнтэзи, драма, комедия",
   },
   {
@@ -60,29 +72,48 @@ let films = [
     genre: "Мультфильм, фэнтэзи, комедия",
   },
   {
+    adult: true,
     time: "16:00",
     name: "Люди в чёрном: Интэрнэшнл",
     genre: "Фантастика, боевик, комедия",
   },
 ];
 
-let tableBody = document.getElementById("t_body");
+tableBody = document.getElementById("t_body");
 
-for (i = 0; i < films.length; i++) {
-  tableBody.innerHTML += `
-<tr class="table__color">
-<td class="checktable">
-    <i>
-        <svg width="11" height="9" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-                d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
-                fill="white" />
-        </svg>
-    </i>
-</td>
-<td>${films[i].time}</td>
-<td>${films[i].name}</td>
-<td>${films[i].genre}</td>
-</tr>
-`;
+const filmHelper = {
+  getTitle() {
+    return this.name;
+  },
+  getTime() {
+    return this.time;
+  },
+  getGeneres() {
+    return this.genre;
+  },
+};
+
+function generatorFilmRow(film) {
+  return `
+  <tr class="table__color">
+  <td class="checktable">
+      <i>
+          <svg width="11" height="9" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M4.60581 6.79378L1.46056 3.93033L0.787354 4.66979L4.70255 8.23421L10.8223 0.94099L10.0562 0.298203L4.60581 6.79378Z"
+                  fill="white" />
+          </svg>
+      </i>
+  </td>
+  <td>${filmHelper.getTime.apply(film)}</td>
+  <td>${filmHelper.getTitle.apply(film)}</td>
+  <td>${filmHelper.getGeneres.apply(film)}</td>
+  </tr>
+  `;
+}
+
+for (film of filmsDescription) {
+  if (!film.adult) {
+    tableBody.innerHTML += generatorFilmRow(film);
+  }
 }
